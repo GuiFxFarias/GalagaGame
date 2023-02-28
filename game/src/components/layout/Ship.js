18a869ee1f009ec17a89d4a0a20f8909cbfe283b
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2023-02-28 11:09:15 -0300

    start project galaga

--- game/src/components/layout/Ship.js
+++ game/src/components/layout/Ship.js
@@ -1,23 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./ShipCss.css";
 
 function Ship() {
-  let posY = 0;
-  let posX = 0;
+  const [Y, setY] = useState(0);
+  const [X, setX] = useState(0);
 
   function moveDown() {
-    console.log(posY);
-    return (posY += 10);
+    setY(Y + 10);
+  }
+
+  function moveRight() {
+    setX(X + 10);
+  }
+
+  function moveRight() {
+    setX(X + 10);
+  }
+
+  function moveRight() {
+    setX(X + 10);
   }
 
   return (
     <>
       <div
         className="ship"
-        style={{ position: "absolute", top: `${posY}%`, left: `${posX}%` }}
+        style={{ position: "absolute", top: `${Y}%`, left: `${X}%` }}
       ></div>
       <div className="dow">
         <button onClick={moveDown}>MoveDown</button>
+        <button onClick={moveRight}>moveRight</button>
       </div>
     </>
   );
